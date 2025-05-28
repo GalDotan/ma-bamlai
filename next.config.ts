48b4beb74adfb22801d81902aa25aaa9d55dd37f
GalDotan <gldotan@gmail.com>
2025-05-28 13:28:16 +0300

    *

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true
+})
 
-export default nextConfig;
+const nextConfig = {
+  // if you're using output: 'export' for static export, add it here
+  reactStrictMode: true,
+  // output: 'export', // optional
+}
+
+module.exports = withPWA(nextConfig)
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,23 @@
-'use client'          // mark this as a client component
 import { Suspense, ReactNode } from 'react'
 import NavBar from '@/components/NavBar'
 import { ToastContainer } from 'react-toastify'
 import './globals.css';
 
+export const metadata = {
+  title: 'MAbmlai',
+  description: 'FRC inventory system',
+  themeColor: '#000000', // this sets the meta tag
+}
+
+
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <head />
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon-192.png" />
+        <meta name="theme-color" content="#000000" />
+      </head>
       <body
         suppressHydrationWarning
         className="bg-surface text-white min-h-screen">
